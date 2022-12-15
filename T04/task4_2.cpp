88d0cd64170868f5f79ef943f95996a870223877
Ivan <ivt.ivanov1@gmail.com>
2022-12-15 22:27:20 +0100

    templates

---
+++ T04/task4_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() { 
+    std::ifstream file("input.txt");
+    std::string str;
+    int total = 0;
+    while (std::getline(file, str)) {
+    }
+}
